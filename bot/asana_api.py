a66d0ff943065e2893f85f15b18c0d6ed14ee2fb
karech <karech@users.noreply.github.com>
2025-03-03 14:47:57 +0400

    Update parsing task link

--- bot/asana_api.py
+++ bot/asana_api.py
@@ -1,4 +1,5 @@
 import re
+from itertools import chain
 
 import asana
 from aioify import aioify
@@ -7,14 +8,18 @@ from bot.settings import settings
 
 
 class AsanaAPI:
-    _task_link_re = re.compile(r'app\.asana\.com/\d+/\d+/(?P<gid>\d+)')
+    _task_link_re = (
+        re.compile(r'app\.asana\.com/\d+/\d+/project/\d+/task/(?P<gid>\d+)'),
+        re.compile(r'app\.asana\.com/\d+/\d+/inbox/\d+/item/(?P<gid>\d+)/story/'),
+        re.compile(r'app\.asana\.com/\d+/\d+/(?P<gid>\d+)'),
+    )
 
     def __init__(self) -> None:
         self._client = asana.Client.access_token(settings.ASANA_TOKEN)
 
     @classmethod
-    def find_task_ids(cls, text: str) -> list[str]:
-        return cls._task_link_re.findall(text)
+    def find_task_ids(cls, text: str) -> set[str]:
+        return set(chain(*map(lambda r: r.findall(text), cls._task_link_re)))
 
     @aioify
     def send_message(self, task_id: str, message: str, pin: bool = False) -> None:
